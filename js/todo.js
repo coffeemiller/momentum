cf7452035123b8abfac5f007215de510b22e4e59
Coffeemiller <dev2freedom@gmail.com>
2022-06-05 21:20:32 +0900

    ToDo List completed function addition

--- js/todo.js
+++ js/todo.js
@@ -17,18 +17,62 @@ function deleteToDo(event) {
     li.remove();
 }
 
+function checkBoxToggle(event) {
+    const li = event.target.parentElement;
+    const targetIndex = parseInt(toDos.findIndex((toDo) => toDo.id === parseInt(li.id)));
+    const originTodo = toDos[targetIndex];
+    const span = li.querySelector("span");
+    const btnChk = event.target;
+    const strike = document.createElement("strike");
+    if (btnChk.checked) {
+        strike.innerText = span.innerText;
+        span.innerText = "";
+        originTodo.completed = true;
+        const msg = document.createElement("span");
+        msg.classList.add('completed')
+        msg.innerText = " - Completed";
+        span.appendChild(strike);
+        span.appendChild(msg);
+    } else {
+        originTodo.completed = false;
+        const getStrike = span.querySelector("strike");
+        span.innerText = getStrike.innerText;
+        getStrike.remove();
+    }
+    saveToDos();
+    console.log(toDos[targetIndex]);
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
     span.innerText = newTodo.text;
+    const btnChk = document.createElement("input");
+    btnChk.type = "checkbox";
+    btnChk.value = newTodo.id;
+    btnChk.classList.add("checkbox");
+    btnChk.checked = newTodo.completed;
+    btnChk.addEventListener("click", checkBoxToggle)
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo)
+    li.appendChild(btnChk);
     li.appendChild(span);
+    if (newTodo.completed) {
+        const strike = document.createElement("strike");
+        strike.innerText = newTodo.text;
+        span.innerText = "";
+        const msg = document.createElement("span");
+        msg.classList.add('completed')
+        msg.innerText = " - Completed";
+        span.appendChild(strike);
+        span.appendChild(msg);
+    }
     li.appendChild(button);
     toDoList.appendChild(li);
 }
+
 
 function handleToDoSubmit(event) {
     event.preventDefault();
@@ -37,6 +81,7 @@ function handleToDoSubmit(event) {
     const newTodoObj = {
         text: newTodo,
         id: Date.now(),
+        completed: false
     };
     toDos.push(newTodoObj);
     paintToDo(newTodoObj);
